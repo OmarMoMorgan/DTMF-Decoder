8622aa1392c9023c93dbdf167f99e0acc4921cb3
ahmedsaber4 <s-ahmed.othman@zewailcity.edu.eg>
2022-05-28 04:08:00 +0200

    dtmfdial added

--- dtmfdial.m
+++ dtmfdial.m
@@ -1,8 +1,7 @@
 function  xx = dtmfdial(keyNames,fs)
-
 dtmf.keys =['1','2','3','A';'4','5','6','B';'7','8','9','C';'*','0','#','D'];
 
-
+%tmp_cmp = reshape(dtmf.keys)
 
 dtmf.colTones = ones(4,1)*[1209,1336,1477,1633];
 dtmf.rowTones = [697;770;852;941]*ones(1,4);
@@ -11,20 +10,23 @@ silence_duration=0.05;
 tone_duration = 0.2;
 xx=[];
 
-%t=0:fs*td1;
-length(dtmf)
-for i=1:length(dtmf)
-    if(keyNames(i)==dtmf.keys)
-        [ii,jj]=find(keyNames(i)==dtmf.keys);
-        signal=cos(2*pi*dtmf.colTones(ii,jj)/fs)+cos(2*pi*dtmf.rowTones(ii,jj)/fs);
-        silence = zeros(1,fs*silence_duration);
-        xx=[xx,silence , signal];
+tt=0:fs*tone_duration;
 
-    else  
-        disp("invalid key name");
+for i=1:length(keyNames)
+    %for j=1:length(dtmf.keys)
+        if(find (keyNames(i)==dtmf.keys))
+            [ii,jj]=find(keyNames(i)==dtmf.keys);
+            signal=cos(2*pi*dtmf.colTones(ii,jj)*tt/fs)+cos(2*pi*dtmf.rowTones(ii,jj)*tt/fs);
+            silence = zeros(1,fs*silence_duration);
+            xx=[xx,silence , signal];
+        
+        else  
+           disp("invalid key name");
+        end
     end
-    
 
-end
-%soundsc(xx,fs);
-%fs = 8000 ; 
+
+soundsc(xx,fs);
+%fs = 8000 ;
+%write the following line in the command
+% dtmfdial('123456789ABCD*#0',8000)
